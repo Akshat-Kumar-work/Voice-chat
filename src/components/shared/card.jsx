c197c9a77ce92db024f2dc48ca683d30906709ab
Akshat <iamakku0.0.0.1@gmail.com>
2023-11-07 15:44:05 +0530

    card execution done

--- src/components/shared/card.jsx
+++ src/components/shared/card.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-
-const card = () => {
+const card = ({heading , para1 , para2 , btnText , SecondlastText , logo , lastText }) => {
   return (
+    <div className=' mt-40 w-[50%] max-w-[1260px] max-width-[90%]  flex flex-col p-5  items-center justify-center  space-y-4 m-3  bg-gray-800 rounded-lg '>
+
+    <div className='flex justify-center flex-col items-center'>
+        <h1 className=' text-xl font-bold '> {heading} </h1>
+        <p className=' text-blue-300'> {para1}</p>
+       <p className=' text-blue-300'> {para2} </p> 
+    </div>
+
     <div>
+        <button className='flex flex-row bg-blue-500 rounded-full p-1'>
+            <span className=' pr-2'>{btnText}</span>
+            {logo}
+        </button>
+    </div>
+
+    <div className='flex flex-col '>
+        <span>{SecondlastText} </span>
+        <Link  className=' text-blue-400 text-center' to="/login"> {lastText} </Link> 
+    </div>
 
     </div>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,33 +1,19 @@
 import React from 'react'
-
-import { Link } from 'react-router-dom'
+import Card from '../components/shared/card'
 import {BsFillArrowRightCircleFill} from "react-icons/bs"
+
+
 const home = () => {
   return (
     <div className=' flex justify-center items-center'> 
 
-<div className=' mt-40 w-[50%] max-w-[1260px] max-width-[90%]  flex flex-col p-5  items-center justify-center  space-y-4 m-3  bg-gray-800 rounded-lg '>
+    <Card heading={"Welcome to Voice-Chat"} para1={"We are working hard to get voice-chat ready for everyone!"}
+      para2={"While we wrap up the finishing youches, we are adding people gradually to make sure nothing breaks:)"}
+      btnText={"Get your username"} logo={<BsFillArrowRightCircleFill />}
+      lastText={"Sign in"}
+      SecondlastText={"Have an invite text?"}
+    />
 
-<div className='flex justify-center flex-col items-center'>
-    <h1 className=' text-xl font-bold '>Welcome to Voice-Chat</h1>
-    <p className=' text-blue-300'> We are working hard to get voice-chat ready for everyone.</p>
-   <p className=' text-blue-300'> While we wrap up the finishing touches, we are adding people gradually
-    to make sure nothing breaks. </p> 
-</div>
-
-<div>
-    <button className='flex flex-row bg-blue-500 rounded-full p-1'>
-        <span className=' pr-2'>Get your username</span>
-        <BsFillArrowRightCircleFill />
-    </button>
-</div>
-
-<div className='flex flex-col '>
-    <span>Have an invite text? </span>
-    <Link  className=' text-blue-400 text-center' to="/login"> Sign in </Link> 
-</div>
-
-</div>
     </div>
   )
 }
